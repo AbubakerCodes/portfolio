e389a0faea7946c7fda80176d668763ba6ef28a4
AbubakerCodes <drbeekox@gmail.com>
2024-01-31 13:27:48 +0300

    Updated projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,14 +1,14 @@
 import projectImg_1 from "/assets/project_1.jpg";
-import projectImg_2 from "/assets/project_2.jpg";
 import projectImg_3 from "/assets/project_3.jpg";
 import projectImg_4 from "/assets/project_4.jpg";
+import projectImg_5 from "/assets/project_5.jpg";
 
 const projects = [
     {
         id: 1,
         imgeUrl: projectImg_1,
         title: "Ecommerce product page",
-        description: "Responsive E-commerce product page with cart functionality.",
+        description: "Responsive E-commerce product page with seamless cart functionality.",
         technologies: ["HTML5", "CSS3", "ES6", "React js", "TailwindCSS"],
         liveUrl: "https://ecomm-product-page.vercel.app",
         gitHubUrl: "https://github.com/AbubakerCodes/ecommerce_product_page"
@@ -17,28 +17,40 @@ const projects = [
         id: 2,
         imgeUrl: projectImg_3,
         title: "Easybank landing page",
-        description: "This is a responsive landing page for Easybank.",
+        description: "This is a responsive landing page for Easybank with a modern design.",
         technologies: ["HTML5", "CSS3", "ES6", "React js", "TailwindCSS"],
         liveUrl: "https://easybank-landing-page24.vercel.app",
         gitHubUrl: "https://github.com/AbubakerCodes/easybank-landing-page"
     },
     {
         id: 3,
-        imgeUrl: projectImg_2,
-        title: "FAQ accordion",
-        description: "Responsive FAQ accordion page built with vanilla css and javscript. ",
-        technologies: ["HTML5", "CSS3", "ES6"],
-        liveUrl: "https://faqaccordionmain.netlify.app",
-        gitHubUrl: "https://github.com/AbubakerCodes/faq-accordion-main"
+        imgeUrl: projectImg_5,
+        title: "Multi-step Subscription Form",
+        description: "Streamlined subscription process with dynamic steps for plan selection, add-ons, and personal details.",
+        technologies: ["HTML5", "CSS3", "ES6", "React js", "TailwindCSS"],
+        liveUrl: "https://multi-step-form99.vercel.app",
+        gitHubUrl: "https://github.com/AbubakerCodes/multi-step-form"
     },
     {
         id: 4,
         imgeUrl: projectImg_4,
         title: "Advice generator app",
-        description: "This random Advice generator app uses the Advice Slip API.",
+        description: "This random Advice generator app is powered by the Advice Slip API.",
         technologies: ["HTML5", "CSS3", "ES6", "React js", "TailwindCSS"],
         liveUrl: "https://advice-generator-app24.vercel.app",
         gitHubUrl: "https://github.com/AbubakerCodes/advice-generator-app"
     },
 ];
 export default projects;
+
+
+// import projectImg_2 from "/assets/project_2.jpg";
+// {
+//     id: 4,
+//     imgeUrl: projectImg_2,
+//     title: "FAQ accordion",
+//     description: "Responsive FAQ accordion page built with vanilla CSS and JavaScript. ",
+//     technologies: ["HTML5", "CSS3", "ES6"],
+//     liveUrl: "https://faqaccordionmain.netlify.app",
+//     gitHubUrl: "https://github.com/AbubakerCodes/faq-accordion-main"
+// },
